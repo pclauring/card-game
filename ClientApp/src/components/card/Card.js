784e4bb28d1cf6d73c5b1a6b29bf0c76bc70ce5a
Ben <bdchodos@gmail.com>
2019-02-22 22:30:15 -0500

    #1 updated Card component to display A,J,Q,, and K

--- ClientApp/src/components/card/Card.js
+++ ClientApp/src/components/card/Card.js
@@ -5,9 +5,24 @@ const Card = ({ value, suit, id, discardCard }) => {
     return (
         //n.b. pass the function
         <div className="game-card" onClick={() => discardCard(id)}>
-            <p>{value}</p>
+            <p>{displayCardValue(value)}</p>
             <p>{suit}</p>
         </div>);
+}
+
+function displayCardValue(value) {
+    switch(value) {
+        case 1:
+            return "A";
+        case 11:
+            return "J";
+        case 12:
+            return "Q";
+        case 13:
+            return "K";
+        default:
+            return value;
+    }
 }
 
 export default Card;
