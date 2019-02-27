10a2b824a3396f8b53cb122bc103a940627a5324
Pierce Lauring <pclauring@gmail.com>
2019-02-26 20:14:51 -0500

    Updated game counter to display total cards of each suit in each loc

--- ClientApp/src/components/game/GameCounter.js
+++ ClientApp/src/components/game/GameCounter.js
@@ -1,29 +1,66 @@
 import React from 'react';
 import './Game.css';
 
-const GameCounter = ({ deck, hand, discard, board}) => {
-    return (
-        <div className="game-counter-container">
-            <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Deck</th>
-      <th scope="col">Discard</th>
-      <th scope="col">Hand</th>
-      <th scope="col">Board</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>{deck.length}</td>
-      <td>{discard.length}</td>
-      <td>{hand.length}</td>
-      <td>{board.length}</td>
-    </tr>
-  </tbody>
-</table>
-         </div>
-        );
+const countSuit = (array, suit) => {
+  var suitCount = array.reduce(function (suitTotal, currentCard) {
+    return suitTotal + (currentCard.suit === suit ? 1 : 0);
+  }, 0);
+  return suitCount;
 }
+
+const GameCounter = ({ deck, hand, discard, board }) => {
+
+  return (
+    <div className="game-counter-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Location</th>
+            <th>Count</th>
+            <th scope="col"><span className="game-counter-icon">♥</span></th>
+            <th scope="col"><span className="game-counter-icon">♦</span></th>
+            <th scope="col"><span className="game-counter-icon">♣</span></th>
+            <th scope="col"><span className="game-counter-icon">♠</span></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Deck</td>
+            <td>{deck.length}</td>
+            <td>{countSuit(deck, "Hearts")}</td>
+            <td>{countSuit(deck, "Diamonds")}</td>
+            <td>{countSuit(deck, "Clubs")}</td>
+            <td>{countSuit(deck, "Spades")}</td>
+          </tr>
+          <tr>
+            <td>Discard</td>
+            <td>{discard.length}</td>
+            <td>{countSuit(discard, "Hearts")}</td>
+            <td>{countSuit(discard, "Diamonds")}</td>
+            <td>{countSuit(discard, "Clubs")}</td>
+            <td>{countSuit(discard, "Spades")}</td>
+          </tr>
+          <tr>
+            <td>Board</td>
+            <td>{board.length}</td>
+            <td>{countSuit(board, "Hearts")}</td>
+            <td>{countSuit(board, "Diamonds")}</td>
+            <td>{countSuit(board, "Clubs")}</td>
+            <td>{countSuit(board, "Spades")}</td>
+          </tr>
+          <tr>
+            <td>Hand</td>
+            <td>{hand.length}</td>
+            <td>{countSuit(hand, "Hearts")}</td>
+            <td>{countSuit(hand, "Diamonds")}</td>
+            <td>{countSuit(hand, "Clubs")}</td>
+            <td>{countSuit(hand, "Spades")}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
 
 export default GameCounter;
