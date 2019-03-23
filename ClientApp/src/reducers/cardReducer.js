d4f9980ccc5347fb57899a7a9f9e4e48a3761834
Pierce Lauring <pclauring@gmail.com>
2019-03-23 00:15:08 -0400

    combine reducer added to card reducer

--- ClientApp/src/reducers/cardReducer.js
+++ ClientApp/src/reducers/cardReducer.js
@@ -1,21 +1,35 @@
 import * as types from '../actions/deckBuilderActionTypes';
 import initialState from './deckBuilderInitialState';
+import { combineReducers } from 'redux';
 
 
-
-export default function cardReducer(state = initialState.cards, action) {
+function cardByIdReducer(state = initialState.cards.ById, action) {
     switch (action.type) {
         case types.UPDATE_CARD: {
             //update cards
             const card = state[action.card.id];
             return {
-                ...state, 
-                [card.id]: {
-                    ...action.card
-                }
+                ...state,
+                    [card.id]: {
+                        ...action.card
+                    }
             };
         }
         default:
-        return state;
+            return state;
     }
 }
+
+function cardAllIdsReducer(state = initialState.cards.AllIds, action){
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+const cardReducer = combineReducers({
+    ById: cardByIdReducer,
+    AllIds: cardAllIdsReducer
+})
+
+export default cardReducer
